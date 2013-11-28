2f7c048fc9d09dd5b36987da41bb1f0e93d07c50
Oliver Derafera <oliverderafera@gmail.com>
2013-11-28 11:13:00 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PNSCms::Application.config.secret_key_base = 'f4167be6889a664c1f8b5ebd2cb773cdb9c5219c936e1ada0c80d15bc5229928e19df274286b76766dbaa61d3c8bee779b1d4e45654f9e2e7af7858d11e57393'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PNSCms::Application.config.session_store :cookie_store, key: '_PNS-CMS_session'
